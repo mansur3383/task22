3c8c48d51ed88a1198073aaa8304111fa620657f
mansur3383 <140749343+mansur3383@users.noreply.github.com>
2024-01-01 19:51:10 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Triangle firstTriangle = new Triangle(7, 3, 6);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Параметры треугольника");
+            Console.ResetColor();
+            Console.WriteLine(firstTriangle);
+            Console.WriteLine($"Периметр треугольника: P = {firstTriangle.CalcPerimeter()} см");
+            Console.WriteLine($"Площадь треугольника S = {firstTriangle.CalcArea():f2} см");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nДемонстрация работы индексатора");
+            Console.ResetColor();
+            Console.WriteLine(firstTriangle[0]);
+            Console.WriteLine(firstTriangle[1]);
+            Console.WriteLine(firstTriangle[2]);
+            Console.WriteLine(firstTriangle[4]);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nДемонстрация перегрузки операций и констант");
+            Console.ResetColor();
+            Console.WriteLine(firstTriangle++);
+            Console.WriteLine(firstTriangle--);
+
+            if (firstTriangle)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Треугольник с заданными длинами сторон существует");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Треугольник с заданными длинами сторон не существует");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine(firstTriangle * 9);
+
+            Console.ReadKey();
+        }
+    }
+}
